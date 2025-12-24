c465d5e3de97032b0a725ce36ccea34b127667e2
Billy Arias <xavier.arias0715@gmail.com>
2025-12-23 23:19:41 -0600

    :iphone: Actualizacion de codigo para responsive :iphone:

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -2,14 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { RelatedPagesComponent } from '../../shared/components/related-pages/related-pages.component';
+import { FeaturedDealsComponent } from '../../shared/components/featured-deals/featured-deals.component';
+import { ContacUsComponent } from '../../shared/components/contac-us/contac-us.component';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
   imports: [
     NavbarComponent,
+    FeaturedDealsComponent,
     RelatedPagesComponent,
-    FooterComponent
+    FooterComponent,
+    ContacUsComponent
   ],
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
