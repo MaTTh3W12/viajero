5f099276dd389980853ca8ab19c7d03a0d446c22
matthew.gaitan <matthew.gaitan@medicamentos.gob.sv>
2025-12-29 22:40:25 -0600

    :art::sparkles: Restructure routing and add authentication components
    - Updated routing to separate public and private routes.
    - Added login and register components with basic functionality.
    - Removed unused components such as About Us, Contact, Coupons, Home, and View Coupons.
    - Introduced AuthService for user authentication and management.
    - Implemented admin guard for route protection.
    - Created new sidebar and topbar components for dashboard layout.
    - Added new assets including images and icons. :art::sparkles:

---
+++ src/app/Public/pages/about-us/about-us.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
+import { RelatedPagesComponent } from '../../../shared/components/related-pages/related-pages.component';
+import { FeaturedDealsComponent } from '../../../shared/components/featured-deals/featured-deals.component';
+import { ContacUsComponent } from '../../../shared/components/contac-us/contac-us.component';
+
+@Component({
+  selector: 'app-about-us',
+  standalone: true,
+  imports: [
+    NavbarComponent,
+    FeaturedDealsComponent,
+    RelatedPagesComponent,
+    FooterComponent,
+    ContacUsComponent
+  ],
+  templateUrl: './about-us.component.html',
+  styleUrls: ['./about-us.component.css']
+})
+export class AboutUsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
